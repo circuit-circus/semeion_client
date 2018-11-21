718fe0056c6d5353208d4658782820650ff76b17
Jesper Fogh <foghjesperjhf@gmail.com>
2018-11-21 14:41:22 +0000

    Started adding sounds to the project. Currently only have the sounds locally, since they are not final. So it is necessary to download test sounds (the ones from Enchanted Plants) if setting this up locally.

--- sem_client.js
+++ sem_client.js
@@ -1,5 +1,6 @@
 // sem_client.js
-const mqtt = require('mqtt')
+const mqtt = require('mqtt');
+const player = require('./play_sound.js')(opts = {});
 var stdin = process.openStdin();
 var configs = require('./configs.js');
 
@@ -14,12 +15,19 @@ var lastIsConnected = false;
 
 var sendDataInterval;
 
+var loopedAudio;
+var oneShotAudio;
+var isOneShotPlaying = false;
+var shouldKeepLooping = true;
+
 client.on('error', (err) => {
   console.log('Error: ' + err);
 });
 
 client.on('connect', () => {
   console.log('Hello world, I am ' + myInfo.name);
+
+  playOneShot('l', 0, 0);
 
   // Get ID
   myInfo.clientId = client.options.clientId;
@@ -42,7 +50,7 @@ client.on('connect', () => {
   lastIsConnected = true;
 
   if(!sendDataInterval) {
-    sendDataInterval = setInterval(sendDataUpdate, 3000);
+    sendDataInterval = setInterval(sendDataUpdate, 1000);
   }
 });
 
@@ -75,19 +83,49 @@ stdin.addListener('data', function(d) {
 
 function sendDataUpdate() {
   console.log('Is connected? ' + client.connected);
+  if(!client.connected) {
+    playOneShot('p', 0, 0);
+  }
 
-  i2cConnect().then(function(msg) {
+  i2cRead().then(function(msg) {
     // Convert the received buffer to a string
     msg = msg.toString('utf8');
     // Remove null bytes, which are used to terminate I2C communication
     msg = msg.substring(0, /\0/.exec(msg).index);
     // Split the string into an array, and convert to Numbers
     msg = msg.split(",").map(Number);
-    console.log("Parsed Python data: " + msg);
-    console.log("Sending data to server...");
+
+    switch (msg[0]) {
+      // Dark
+      case 0:
+        playOneShot('k', 0, 11);
+        break;
+      // Idle
+      case 1:
+        playOneShot('k', 0, 11);
+        break;
+      // Interact
+      case 2:
+        let audioId = Math.floor(mapNumber(msg[2], 0, 15, 0, 5));
+        console.log(audioId);
+        playOneShot('be', audioId, audioId);
+        break;
+      // Climax
+      case 3:
+        playOneShot('i', 0, 0);
+        break;
+      // Shock
+      case 4:
+        playOneShot('i', 3, 3);
+        break;
+      default:
+        break;
+    }
+
+    // console.log("Parsed Python data: " + msg);
+    // console.log("Sending data to server.");
     var dataToSend = JSON.stringify({clientInfo: myInfo, clientData: msg})
     client.publish('sem_client/data', dataToSend);
-    console.log("Python data sent to server.");
   })
   .catch(function(error) {
     console.log(error);
@@ -105,7 +143,7 @@ function handleOtherStateRequest (message) {
 
   if(otherState != state) {
     state = otherState + '_OTHER';
-    console.log('Recieved state update. So now my state is ' + state);
+    console.log('Received state update. So now my state is ' + state);
   } else {
     console.log('The others were told that I am ' + state);
   }
@@ -172,18 +210,93 @@ function handleAppExit (options, err) {
   }, 500);
 }
 
+// AUDIO PLAYING
+function playSound(filePrefix, fileNumMin, fileNumMax, shouldLoop) {
+  return new Promise(function(resolve, reject) {
+    let fileName = filePrefix + getRandomInt(fileNumMin, fileNumMax) + '.mp3';
+    if(shouldLoop) {
+      loopedAudio = player.play('sfx/' + fileName, function(err) {
+        if(err) {
+          reject('Can\'t play ' + fileName + ': ' + err);
+        }
+        else {
+          resolve('Successfully played ' + fileName);
+        }
+      });
+    }
+    else {
+      oneShotAudio = player.play('sfx/' + fileName, function(err) {
+        if(err) {
+          reject('Can\'t play ' + fileName + ': ' + err);
+        }
+        else {
+          resolve('Successfully played ' + fileName);
+        }
+      });
+    }
+  })
+}
+
+function playLoop(filePrefix, fileNumMin, fileNumMax) {
+  if(shouldKeepLooping) {
+    playSound(filePrefix, fileNumMin, fileNumMax, true).then(function(msg) {
+      playLoop(filePrefix, fileNumMin, fileNumMax);
+      console.log(msg);
+    })
+    .catch(function(err) {
+      console.log('Error in loop sound: ' + err);
+    });
+  }
+}
+
+function stopLoop() {
+  shouldKeepLooping = false;
+}
+
+function playOneShot(filePrefix, fileNumMin, fileNumMax) {
+  if(!isOneShotPlaying) {
+   isOneShotPlaying = true;
+   playSound(filePrefix, fileNumMin, fileNumMax, false).then(function(msg) {
+     console.log(msg);
+     isOneShotPlaying = false;
+   })
+   .catch(function(err) {
+     console.log('Error in one shot sound: ' + err);
+     isOneShotPlaying = false;
+   });
+  }
+}
+
+function sendCommandToAudio(proc, key) {
+  try {
+    proc.stdin.write(key);
+  }
+  catch (err) {
+    console.log(err);
+  }
+}
+
+// Max is inclusive
+function getRandomInt(min, max) {
+  return min + Math.floor(Math.random() * Math.floor(max + 1));
+}
+
+function mapNumber(x, in_min, in_max, out_min, out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 // PYTHON CONNECTION
-let i2cPyPath = __dirname + '/semeion.py';
-// Connects to i2c
-function i2cConnect() {
-  console.log('Connecting to Python');
+let i2cReadPath = __dirname + '/read_i2c.py';
+// Connects via i2c to read data from the Arduino
+function i2cRead() {
+  // console.log('Connecting to Python');
   return new Promise(function(resolve, reject) {
     var spawn = require('child_process').spawn;
-    var process = spawn('python3', [i2cPyPath]);
+    var process = spawn('python3', [i2cReadPath]);
 
     process.stdout.on('data', function (data) {
       process.kill();
-      console.log("Successfully connected to Python. Parsing data...");
+      // console.log('Successfully connected to Python. Parsing data...');
       resolve(data);
     });
 
